b3cfd4fa9def9371492aa392dd34eee02a9abe89
NicolasPlovie <nicolas@debian>
2017-03-17 18:07:58 +0100

    Chelou

--- CLASS/test.php
+++ CLASS/test.php
@@ -4,12 +4,14 @@ session_start();
 
 include_once("User.class.php");
 include_once("../connect_db.php");
-include_once("login.class.php");
+include_once("Login.class.php");
 $bdd=connect_db("localhost","root","root","3306","pool_php_rush");
-$test = new User("","","test@test.com",,"12345");
+$test = new User("","","test@test.com",12,"12345");
 var_dump($test);
 //$test->addUser($bdd);
-if(!$test->validAttributs())
+
   foreach ($test->getErrors() as $value) {
     print_r ($value);
   }
+Login::setSession('test@test.com');
+Login::setCookie('test@test.com');
