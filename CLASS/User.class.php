f6b966b1a72f52f868da0bee282bdddfc4591897
NicolasPlovie <nicolas@debian>
2017-03-17 14:36:00 +0100

    Correction et validation Class User ajout method validAttributs

--- CLASS/User.class.php
+++ CLASS/User.class.php
@@ -1,6 +1,5 @@
 <?php
 
-
 class User
 {
 //-------------------------------------------------------------------------------------------------------------ATTRIBUTE
@@ -9,30 +8,36 @@ protected $lastname;
 protected $email;
 protected $age;
 protected $password;
+protected $array_errors;
 //---------------------------------------------------------------------------------------------------------------CONSTRUCTOR
 	function __construct($firstname = NULL, $lastname = NULL, $email = NULL, $age = NULL, $password = NULL)
 	{
-		$array_errors=[];
+		$this->array_errors=[];
 		if(!$this->setFirstname($firstname))
-			array_push($array_errors,"Firstname invalid.");
-		
+			array_push($this->array_errors, array('firstname'=>"Firstname invalid."));
+
 		if(!$this->setLastname($lastname))
-			array_push($array_error, "Lastname invalid.");
+			array_push($this->array_errors, array('lastname'=>"Lastname invalid."));
 
 		if(!$this->setEmail($email))
-			array_push($array_error, "Email invalid");
-		
-		if(!$this->setAge($age))
-			array_push($array_error, "Age invalid");
-		
-		if(!$this->setPassword($password))
-			array_push($array_error, "Password invalid");
+			array_push($this->array_errors, array('email'=>"Email invalid"));
 
-		if(count($array_error) > 0)
-			return $array_error;
+		if(!$this->setAge($age))
+			array_push($this->array_errors, array('age'=>"Age invalid"));
+
+		if(!$this->setPassword($password))
+			array_push($this->array_errors, array('password'=>"Password invalid"));
+
 
 	}
 //---------------------------------------------c--------------------------------------------------------------------------METHODS
+	public function validAttributs()
+	{
+		if(count($this->array_errors) == 0)
+			return true;
+		else
+			return false;
+	}
 
 	public function confirmPassword($S_confirmPassword)
 	{
@@ -40,14 +45,14 @@ protected $password;
 		{
 			return true;
 		}
-		else 
+		else
 			return false;
 	}
 
 	public function addUser($S_bdd)
 	{
 		$req = $S_bdd->prepare("INSERT INTO users VALUES('',:lastname,:firstname,:age,:email,:password,0)");
-		$bool = $rep->execute(array(
+		$bool = $req->execute(array(
 			':lastname'=> $this->lastname,
 			':firstname'=> $this->firstname,
 			':age'=> $this->age,
@@ -57,7 +62,7 @@ protected $password;
 	}
 
 
-	
+
 //-----------------------------------------------------------------------------------------------------------------------------GETTER
 	function getFirstname()
 	{
@@ -79,42 +84,60 @@ protected $password;
 	{
 		return $this->password;
 	}
+	function getErrors()
+	{
+		return $this->array_errors;
+	}
+
 //-----------------------------------------------------------------------------------------------------------------------------SETTER
 
 	function setFirstname($S_firstname)
 	{
 		if(!is_int($S_firstname) && strlen($S_firstname) > 1)
-			$this->firstname = $S_firstname;
+		{
+			 $this->firstname = $S_firstname;
+			 return true;
+		}
 		else
 			return false;
 	}
 	function setLastname($S_lastname)
 	{
 		if(!is_int($S_lastname) && strlen($S_lastname) > 1)
+		{
 			$this->lastname = $S_lastname;
+			return true;
+		}
 		else
 			return false;
 	}
 	function setEmail($S_email)
 	{
 		if(preg_match('#^[A-Za-z0-9.-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$#', $S_email))
+		{
 			$this->email = $S_email;
+			return true;
+		}
 		else
 			return false;
 	}
 	function setAge($S_age)
 	{
 		if(is_int($S_age) && $S_age >= 13)
+		{
 			$this->age = $S_age;
+			return true;
+		}
 		else
 			return false;
-		
+
 	}
 	function setPassword($S_password)
 	{
 		if (!is_int($S_password) && strlen($S_password) >= 5)
 		{
 			$this->password = $S_password;
+			return true;
 		}
 		else
 			return false;
