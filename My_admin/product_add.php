6b8bf0c5c9b28eb0ce1f3c468c7bdb5020c97040
NicolasPlovie <nicolas@debian>
2017-03-20 14:51:35 +0100

    ajout user list and delete

---
+++ My_admin/product_add.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+include_once('../CLASS/Login.class.php');
+include_once('../connect_db.php');
+$bdd=connect_db("localhost","root","root","3306","pool_php_rush");
+Login::isUserLogged();
+Login::isAdmin($bdd);
+?>
+<!Doctype html>
+<html>
+  <head>
+    <title>Home Admin</title>
+  </head>
+  <body>
+    <header>
+      <h1> Home Admin>
+    </header>
+    <main>
+      <nav>
+        <ul id="first">
+          <li>
+            <ul id="second">
+              <li><h1> USER<h1></li>
+              <li><a href ='user_form.php'>Add user</a></li>
+              <li><a href ='user_list.php'>Modify user</a></li>
+            </ul>
+          </li>
+          <li>
+            <ul id="second">
+              <li><h1> Product<h1></li>
+              <li><a href ='product_add.php'>Add product</a></li>
+              <li><a href ='product_list.php'>Modify product</a></li>
+            </ul>
+          </li>
+      </nav>
+    <article>
+
+    </article>
+    </main>
+    <footer>
+    </footer>
+  </body>
+</html>
