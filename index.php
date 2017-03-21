1371fe0bf4d722482d232ef5ee3820ec39831f2e
Johnnyshaun <johnnyshaun@changeme>
2017-03-21 17:03:36 +0100

    index change

--- index.php
+++ index.php
@@ -7,32 +7,7 @@ include_once('start_file.php');
 		header('Location: login.php');
 	}
 
+header('Location: product.php');
+
 ?>
 
-<!DOCTYPE HTML>
-<html>
-	<head>
-		<title>
-			Mon index PHP
-		</title>
-	</head>
-
-	<body>
-		<header>
-
-		</header>
-
-		<main>
-			<form method="POST">
-
-	  			<label for="form_submit"></label>
-				<input id="form_submit" type="submit" name ="send" value="Logout">
-			</form>
-
-		</main>
-
-		<footer>
-
-		</footer>
-	</body>
-</html>
